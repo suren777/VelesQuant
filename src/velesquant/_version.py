9eaca40d8143a331500661e88cb27f9597065949
suren777 <suren.islyaev@gmail.com>
2026-01-22 11:28:32 +0000

    update

--- src/velesquant/_version.py
+++ src/velesquant/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev29+gf8767fff6.d20260122'
-__version_tuple__ = version_tuple = (0, 1, 'dev29', 'gf8767fff6.d20260122')
+__version__ = version = '0.1.dev30+g7066ebb5f.d20260122'
+__version_tuple__ = version_tuple = (0, 1, 'dev30', 'g7066ebb5f.d20260122')
 
-__commit_id__ = commit_id = 'gf8767fff6'
+__commit_id__ = commit_id = 'g7066ebb5f'
